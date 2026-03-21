e7800330f75a8143640a64c7b6d3874da5e18412
paulmug1000 <paul@gothrive.uk>
2026-03-21 08:28:28 +0000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
+  experimental: {
+    isrMemoryCacheSize: 52 * 1024 * 1024,
+  },
 }
-
-module.exports = nextConfig

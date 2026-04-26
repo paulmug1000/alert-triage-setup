e6e7971666834acdb69f342d1a722453fba01e04
paulmug1000 <paul@gothrive.uk>
2026-04-26 07:50:36 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,18 @@
+import Head from "next/head";
 import MainMenu from "./menu";
 
 export default function Index() {
-  return <MainMenu />;
+  return (
+    <>
+      <Head>
+        <title>Pulse Triage System</title>
+        <link rel="icon" href="https://pulsedashboard.co.uk/wp-content/uploads/2026/03/pulsefavicon.png" />
+        <link rel="apple-touch-icon" href="https://pulsedashboard.co.uk/wp-content/uploads/2026/03/pulsefavicon.png" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="apple-mobile-web-app-title" content="Pulse Triage" />
+      </Head>
+      <MainMenu />
+    </>
+  );
 }

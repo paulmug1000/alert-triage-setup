ec2602ffd2446e8ee212a8fab86a5fb732958511
paulmug1000 <paul@gothrive.uk>
2026-09-25 12:57:22 +0100

    title

--- pages/index.js
+++ pages/index.js
@@ -5,14 +5,14 @@ export default function Index() {
   return (
     <>
       <Head>
-        <title>PMS - Pulse Management System</title>
+        <title>PMA - Pulse Management Application</title>
         <link rel="icon" href="https://pulsedashboard.co.uk/wp-content/uploads/2026/03/pulsefavicon.png" />
         <link rel="apple-touch-icon" href="https://pulsedashboard.co.uk/wp-content/uploads/2026/03/pulsefavicon.png" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="default" />
         <meta name="apple-mobile-web-app-title" content="Pulse Management System" />
       </Head>
-      <TriageSystem onBack={() => {}} />
+      <TriageSystem onBack={() => { }} />
     </>
   );
 }
